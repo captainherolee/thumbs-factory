cd30e687cc8299d2bc4f6af4ab4e9b64de44e1c4
captaintech <captaintech.hero@gmail.com>
2023-07-17 16:38:13 +0900

    add basic folder

---
+++ thumbs-server/app.js
@@ -0,0 +1,11 @@
+var express = require('express')
+
+var app = express()
+
+app.get('/', function(req,res) {
+    res.send("<h1>hi friend!</h1>")
+})
+
+app.listen(2000, function() {
+    console.log("start! express server on port 2000")
+})
