aa28769b7dcfc0c02eccdfdd69729e72c21021f0
captaintech <captaintech.hero@gmail.com>
2023-07-17 17:14:47 +0900

    add pretty policy

--- thumbs-web/src/App.tsx
+++ thumbs-web/src/App.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
 
 function App() {
-  console.log("sdfdsf")
   return (
     <div className="App">
       <header className="App-header">
-        
-        <p>
-         React Web
-        </p>
-
+        <p>React Web</p>
       </header>
     </div>
   );
