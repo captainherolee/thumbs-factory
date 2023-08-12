92ae378dd9fb0524a8b3708e7e508f92914c2e99
captaintech <captaintech.hero@gmail.com>
2023-08-12 16:57:42 +0900

    update server

--- thumbs-server/app.js
+++ thumbs-server/app.js
@@ -1,12 +1,41 @@
-var express = require('express')
+const express = require("express");
+const mariadb = require("mariadb");
 
-var app = express()
+const app = express();
+const port = 3000;
 
-app.get('/', function(req,res) {
-    res.send("<h1>hi friend!</h1>")
-})
+// MariaDB 연결 설정
+const pool = mariadb.createPool({
+  host: "localhost", // 호스트 주소
+  user: "your_username", // 사용자 이름
+  password: "your_password", // 비밀번호
+  database: "your_database", // 데이터베이스 이름
+});
 
+// API 엔드포인트
+app.get("/api/data", (req, res) => {
+  pool
+    .getConnection()
+    .then((conn) => {
+      conn
+        .query("SELECT * FROM your_table")
+        .then((rows) => {
+          conn.release();
+          res.json(rows);
+        })
+        .catch((err) => {
+          conn.release();
+          console.error("Query error:", err);
+          res.status(500).json({ error: "Error querying the database" });
+        });
+    })
+    .catch((err) => {
+      console.error("Connection error:", err);
+      res.status(500).json({ error: "Error connecting to the database" });
+    });
+});
 
-app.listen(2000, function() {
-    console.log("start! express server on port 2000")
-})
+// 서버 시작
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
