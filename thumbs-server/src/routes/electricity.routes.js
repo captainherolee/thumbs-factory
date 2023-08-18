734d6a8a00e1433a20635841dc2e5db6d7dfc906
captaintech <captaintech.hero@gmail.com>
2023-08-19 00:10:16 +0900

    remove unnecessary log

--- thumbs-server/src/routes/electricity.routes.js
+++ thumbs-server/src/routes/electricity.routes.js
@@ -1,5 +1,4 @@
 module.exports = (app) => {
-  console.log("here");
   const electricity = require("../controllers/electricity.controller.js");
   app.get("/electricity", electricity.all);
   app.get("/electricity/status", electricity.status);
