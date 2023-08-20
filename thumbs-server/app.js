76400c712d2d99a273c12c568695bb578e17ac0a
captaintech <captaintech.hero@gmail.com>
2023-08-20 11:15:32 +0900

    fixed cors issue

--- thumbs-server/app.js
+++ thumbs-server/app.js
@@ -5,11 +5,16 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-const port = process.env.SERVER_PORT || 3001;
-
+app.set("port", process.env.SERVER_PORT || 3001);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 
 app.get("/ping", (req, res) => {
   res.json({ message: "ok" });
@@ -19,6 +24,6 @@ require("./src/routes/electricity.routes.js")(app);
 require("./src/routes/gas.routes.js")(app);
 require("./src/routes/log.routes.js")(app);
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}.`);
+app.listen(app.get("port"), () => {
+  console.log("server started - port: " + app.get("port"));
 });
